9f87cc130e13bb36a25e2170dd0d445a56d6f2f9
Felipe Nery <felipenerypaulino@gmail.com>
2016-12-02 15:46:33 -0200

    Terminado API 100%

--- Fiap.Exemplo02.Service/Controllers/AlunoController.cs
+++ Fiap.Exemplo02.Service/Controllers/AlunoController.cs
@@ -21,10 +21,10 @@ namespace Fiap.Exemplo02.Service.Controllers
         public Aluno Get(int id)
         {
             return _unit.AlunoRepository.BuscarPorId(id);
-
         }
+
         public IHttpActionResult Post(Aluno aluno)
-        {
+        { 
             if (ModelState.IsValid)
             {
                 _unit.AlunoRepository.Cadastrar(aluno);
@@ -37,6 +37,28 @@ namespace Fiap.Exemplo02.Service.Controllers
                 return BadRequest(ModelState);
             }
         }
-        
+
+        public void Delete(int id)
+        {
+            _unit.AlunoRepository.Remover(id);
+            _unit.Salvar();
+        }
+        public IHttpActionResult Put(int id, Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                aluno.Id = id;
+                _unit.AlunoRepository.Alterar(aluno);
+                _unit.Salvar();
+                return Ok(aluno);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+
+
     }
 }
