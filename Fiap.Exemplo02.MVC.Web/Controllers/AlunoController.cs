49180a70eca654f95413310372dec23db16385bf
Felipe Nery <alunoshift@fiap.com.br>
2016-12-06 17:22:25 -0200

    Adicionado mensagem de erro no aluno controle _salavar

--- Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
+++ Fiap.Exemplo02.MVC.Web/Controllers/AlunoController.cs
@@ -107,7 +107,17 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
                     GrupoId = alunoViewModel.GrupoId
                 };
                 _unit.AlunoRepository.Cadastrar(aluno);
-                _unit.Salvar();
+                try
+                {
+                    _unit.Salvar();
+                }
+                catch(Exception e)
+                {
+                    alunoViewModel.Mensagem = "Erro - " + e.Message; //msg erro
+                    alunoViewModel.ListaGrupo = ListarGrupos();
+                    return View(alunoViewModel);
+                }
+                
                 return RedirectToAction("Cadastrar", new { msg = "Aluno Cadastrado" });
             }
             else
