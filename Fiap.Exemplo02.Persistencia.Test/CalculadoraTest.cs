7a59653c1b16287c79feab75c481cbf0b77a75b2
Felipe Nery <alunoshift@fiap.com.br>
2016-12-06 17:49:33 -0200

    add projeto para teste

---
+++ Fiap.Exemplo02.Persistencia.Test/CalculadoraTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fiap.Exemplo02.Persistencia.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void Calcular_Fatorial_Ok()
+        {
+            //Inicializar os obj
+
+            Calculadora calc = new Calculadora();
+
+            //Chamar o metodo que sera testado
+
+            var resultado = calc.Fatorial(5);
+
+            //Validar o resultado
+
+            Assert.AreEqual(120, resultado);
+        }
+    }
+}
