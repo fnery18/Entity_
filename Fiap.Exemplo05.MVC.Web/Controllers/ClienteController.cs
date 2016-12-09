8e4abf3c52c1694ef35d5cd6702e3771959bd1d6
Aluno Shift <alunoshift@fiap.com.br>
2016-12-09 17:22:23 -0200

    add projeto com sis autentific

---
+++ Fiap.Exemplo05.MVC.Web/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo05.MVC.Web.Controllers
+{
+    [Authorize]
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
