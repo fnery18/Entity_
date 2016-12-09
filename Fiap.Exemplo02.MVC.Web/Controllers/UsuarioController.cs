92d2cfaa6174cf26ab3bd0d3c82314349d899f14
Aluno Shift <alunoshift@fiap.com.br>
2016-12-09 18:58:17 -0200

    Terminado usuario controller, falta fazer views login/regist

--- Fiap.Exemplo02.MVC.Web/Controllers/UsuarioController.cs
+++ Fiap.Exemplo02.MVC.Web/Controllers/UsuarioController.cs
@@ -1,9 +1,11 @@
 ﻿using Fiap.Exemplo02.Dominio.Models;
 using Fiap.Exemplo02.MVC.Web.App_Start;
+using Fiap.Exemplo02.MVC.Web.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,6 +39,68 @@ namespace Fiap.Exemplo02.MVC.Web.Controllers
                 return Url.Action("index", "home");
             }
             return returnUrl;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(UsuarioViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var user = new Usuario
+            {
+                UserName = model.Email
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var identity = await userManager.CreateIdentityAsync(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+                GetAuthenticationManager().SignIn(identity);
+                return RedirectToAction("index", "home");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new UsuarioViewModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> LogIn(UsuarioViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var user = await userManager.FindAsync(model.Email, model.Password);
+            if (user != null)
+            {
+                var identity = await userManager.CreateIdentityAsync(
+                user, DefaultAuthenticationTypes.ApplicationCookie);
+                GetAuthenticationManager().SignIn(identity);
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+            ModelState.AddModelError("", "Usuário e/ou Senha inválidos");
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            GetAuthenticationManager().SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("index", "home");
         }
 
     }
